b600cddd78e7247b1d070a44f961b17c679b002b
Koby Samuel <ksamuel890@gmail.com>
2017-05-10 13:45:19 -0400

    blah

---
+++ src/helloworld2014/Helloworld2014.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld2014;
+
+import java.awt.Component;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Koby Samuel
+ */
+public class Helloworld2014 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        coins();
+    }
+    private static void coins() {
+        int cents;
+    }
+}
